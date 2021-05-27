778bbc3ed42f6f6e266110574b65fab1dce40787
Ed <relentlessreed@gmail.com>
2021-05-26 23:12:02 -0500

    Added and Linked CSS file/ Updated Styling

--- index.js
+++ index.js
@@ -148,10 +148,11 @@ function htmlHead() {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Team Profiles</title>
+        <link rel="stylesheet" href="/dist/style.css">
+    <title>Team Profile Generator</title>
 </head>  
 <body>
-    <section class="jumbotron jumbotron-fluid bg-info text-white text-center">
+    <section class="jumbotron">
         <div class="container">
             <h3 class="display-3">My Team</h3>
         </div>
